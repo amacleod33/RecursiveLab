172eeae7f5c29a55a54d97cc36880cc7595373fd
Alexander <alexander.macleod.18@cnu.edu>
2019-11-21 15:12:44 -0500

    finished path

--- Gradle/src/main/java/Path.java
+++ Gradle/src/main/java/Path.java
@@ -25,27 +25,37 @@ public class Path {
         if (array[row][column] == target) {
             return "";
         }
-        else {
-            if (column + 1 < array[0].length) {
-                return path(array, row, column + 1, target) + "<";
-
-            }
-            else if (row + 1 < array[1].length) {
-                return path(array, row + 1, 0, target) + "v";
-
-            }
-            else if (column - 1 > array[0].length) {
-                return path(array, row, column + 1, target) + ">";
-
-            }
-            else if (row - 1 > array[1].length) {
-                return path(array, row - 1, column, target) + "^";
-
-            }
-            else {
-                return "nah";
-            }
+        
+        if (row - 1 < 4 && row - 1 >= 0
+            && array[row - 1][column] == array[row][column] + 1) {
+            return "^" + path(array, row - 1, column, target);
+        }
+        
+        if (row + 1 < 4
+            && array[row + 1][column] == array[row][column] + 1) {
+            return "v" + path(array, row + 1, column, target);
 
         }
+        
+       
+        if (column + 1 < 4
+            && array[row][column + 1] == array[row][column] + 1) {
+            return ">" + path(array, row, column + 1, target);
+
+        }
+        
+        if (column - 1 < 4 && column - 1 >= 0
+            && array[row][column - 1] == array[row][column] + 1) {
+            return "<" + path(array, row, column - 1, target);
+        }
+        
+       
+       
+
+
+        
+        return "";
+
     }
+
 }
